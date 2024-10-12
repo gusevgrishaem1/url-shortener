abfeef141ba4558bbedbd4686fd9d49090d5e60e
gusev.grigoriy5 <gusev.grigoriy5@wb.ru>
2024-10-12 23:54:38 +0400

    правки в ui-странице сервиса, в бд, и в деплое

--- db_scripts/001_create_tables.sql
+++ db_scripts/001_create_tables.sql
@@ -2,9 +2,7 @@ CREATE TABLE if not exists urls
 (
     id           SERIAL PRIMARY KEY,
     original_url TEXT NOT NULL,
-    short_url    TEXT NOT NULL UNIQUE
+    short_url    TEXT NOT NULL UNIQUE,
+    create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-ALTER TABLE urls
-    ADD COLUMN create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
