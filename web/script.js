4e3ff15620814895a9f02847cb0c6d33041a1c35
New Username <newemail@example.com>
2024-11-27 22:56:28 +0400

    set backendUrl

--- web/script.js
+++ web/script.js
@@ -1,4 +1,4 @@
-const backendUrl = "http://url-shortener-production-8874.up.railway.app:8080";
+const backendUrl = "http://url-shortener-production-8874.up.railway.app";
 
 document.getElementById('submit-button').addEventListener('click', async function (event) {
     event.preventDefault();
