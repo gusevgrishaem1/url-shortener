774ea33a74ba325232d7077ae2eb335d6f08f65c
New Username <newemail@example.com>
2024-12-10 22:18:38 +0400

    db: 1) create index for column short_url

--- db_scripts/001_create_tables.sql
+++ db_scripts/001_create_tables.sql
@@ -5,3 +5,5 @@ CREATE TABLE if not exists urls
     short_url    TEXT NOT NULL UNIQUE,
     create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX IF NOT EXISTS idx_short_url ON urls(short_url);
